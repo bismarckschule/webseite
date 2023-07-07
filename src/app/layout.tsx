9b714044db303dfabc0dc59bf556bd81604aab68
Kiyan Menzel <kiyan.menzel@gmail.com>
2023-07-07 21:32:50 +0200

    Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,39 +23,6 @@ export default function RootLayout({
         </style>
       </head>
       <body className={inter.className}>{children}
-        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"></link>
-          <nav id="navbar" className="">
-            <div className="nav-wrapper">
-              <div className="logo">
-                <a href="#home"><i className="fa fa-angellist"></i> Logo</a>
-              </div>
-
-              <ul id="menu">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-              </ul>
-            </div>
-          </nav>
-
-
-          <div className="menuIcon">
-            <span className="icon icon-bars"></span>
-            <span className="icon icon-bars overlay"></span>
-          </div>
-
-
-          <div className="overlay-menu">
-            <ul id="menu">
-              <li><a href="#home">Home</a></li>
-              <li><a href="#services">Services</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-          </div>
-
-          <script src="navbar.js"></script>
       </body>
     </html>
   )
